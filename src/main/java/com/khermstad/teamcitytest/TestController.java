c2714807bbecdd59427181d4cb1fcca35c71a4fb
Kris Hermstad <krishermstad@gmail.com>
2019-03-03 16:20:31 -0500

    fixing rest controller

--- src/main/java/com/khermstad/teamcitytest/TestController.java
+++ src/main/java/com/khermstad/teamcitytest/TestController.java
@@ -1,12 +1,14 @@
 package com.khermstad.teamcitytest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
     @GetMapping("/test")
+    @ResponseBody
     public String indexRest(){
         return "HELLO!";
     }
