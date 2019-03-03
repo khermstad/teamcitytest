6851a51840c0895f8009adec0b6b0e95b28e531b
Kris Hermstad <krishermstad@gmail.com>
2019-03-03 16:14:53 -0500

    changed restcontroller name

--- src/main/java/com/khermstad/teamcitytest/TestController.java
+++ src/main/java/com/khermstad/teamcitytest/TestController.java
@@ -1,10 +1,10 @@
 package com.khermstad.teamcitytest;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-public class RestController {
+@RestController
+public class TestController {
 
     @GetMapping("/test")
     public String indexRest(){
